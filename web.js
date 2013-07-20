2c9557bbf00ae5e8f6fe7ad7c240fe40eab056b4
Gaurav Nirwal <gnirwal@gmail.com>
2013-07-20 11:49:49 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var buf = new Buffer(256);
+
+buf = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//console.log("the string is ", buf.toString('utf8',0));
